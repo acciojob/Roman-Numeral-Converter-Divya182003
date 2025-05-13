9dd181ef97fc2134fbfb985f7019224fe7333991
Divya182003 <divyabhootda05@gmail.com>
2025-05-13 14:05:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-const romanMap= {   
-0:['M',1000], 
-1:['CM',900],
-2:['D',500],
-3:['CD',400],
-4:['C',100],
-5:['XC',90],
-6:['L',50],
-7:['XL',40],
-8:['X',10],
-9:['IX',9],
-10:['V',5],
-11:['IV',4],
-12:['I',1],
-};
+function convertToRoman(num) {
+  const romanMap = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
-let n=798;
-let ans=""
-while(n!=0)
-{
-    for(let i in romanMap)
-    {
-        let value=romanMap[i][0];
-        let num=romanMap[i][1]
-         if(num<=n)
-          {
-          ans=ans+value;
-          n=n.num;
-          break;
-      }
+  let result = "";
+
+  for (let [symbol, value] of romanMap) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
     }
+  }
 
+  return result;
+}
 
+console.log(convertToRoman(36)); // Output: XXXVI
+module.exports = convertToRoman;
