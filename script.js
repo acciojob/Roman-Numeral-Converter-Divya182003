fe0796a2831ce4cdf8e2c2560704b5a95c8d8cd9
Divya182003 <divyabhootda05@gmail.com>
2025-05-13 13:23:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Number to Roman Numeral Converter</title>
-</head>
-<body>
+const romanMap= {   
+0:['M',1000], 
+1:['CM',900],
+2:['D',500],
+3:['CD',400],
+4:['C',100],
+5:['XC',90],
+6:['L',50],
+7:['XL',40],
+8:['X',10],
+9:['IX',9],
+10:['V',5],
+11:['IV',4],
+12:['I',1],
+};
 
-<script>
-function intToRoman(num) {
-    if (num <= 0 || num > 100000) {
-        return "Number out of range (1-100000)";
+let n=798;
+let ans=""
+while(n!=0)
+{
+    for(let i in romanMap)
+    {
+        let value=romanMap[i][0];
+        let num=romanMap[i][1]
+         if(num<=n)
+          {
+          ans=ans+value;
+          n=n.num;
+          break;
+      }
     }
-
-    const romanNumerals = [
-        ['M', 1000],
-        ['CM', 900], // Added for efficiency
-        ['D', 500],
-        ['CD', 400], // Added for efficiency
-        ['C', 100],
-        ['XC', 90],  // Added for efficiency
-        ['L', 50],
-        ['XL', 40],  // Added for efficiency
-        ['X', 10],
-        ['IX', 9],   // Added for efficiency
-        ['V', 5],
-        ['IV', 4],  // Added for efficiency
-        ['I', 1]
-    ];
-
-    let result = '';
-    for (let i = 0; i < romanNumerals.length; i++) {
-        const symbol = romanNumerals[i][0];
-        const value = romanNumerals[i][1];
-
-        while (num >= value) {
-            result += symbol;
-            num -= value;
-        }
-    }
-    return result;
-}
-</script>
-
-</body>
-</html>
 
 
