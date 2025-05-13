45fd1add1e32514c880cfbc39a91926580b86007
Divya182003 <divyabhootda05@gmail.com>
2025-05-13 12:56:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+<!DOCTYPE html>
+<html>
+<head>
+<title>Number to Roman Numeral Converter</title>
+</head>
+<body>
 
-  //your code here
+<script>
+function intToRoman(num) {
+    if (num <= 0 || num > 100000) {
+        return "Number out of range (1-100000)";
+    }
 
+    const romanNumerals = [
+        ['M', 1000],
+        ['CM', 900], // Added for efficiency
+        ['D', 500],
+        ['CD', 400], // Added for efficiency
+        ['C', 100],
+        ['XC', 90],  // Added for efficiency
+        ['L', 50],
+        ['XL', 40],  // Added for efficiency
+        ['X', 10],
+        ['IX', 9],   // Added for efficiency
+        ['V', 5],
+        ['IV', 4],  // Added for efficiency
+        ['I', 1]
+    ];
+
+    let result = '';
+    for (let i = 0; i < romanNumerals.length; i++) {
+        const symbol = romanNumerals[i][0];
+        const value = romanNumerals[i][1];
+
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+    return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+</script>
 
-// console.log(convertToRoman(36));
+</body>
+</html>
 
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
